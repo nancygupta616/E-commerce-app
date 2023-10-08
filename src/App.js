ea0f8776f2096a6e72b493db0cf156a3d61cbdc5
nancy.gupta <nancy.gupta@decimal.co.in>
2023-10-08 12:53:40 +0530

    new-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        <Router>
+          <Routes>          
+              <Route path="/" element={<ProductListing />} />
+              <Route path="/product/:productId" element={<ProductDetail />} />
+              <Route> 404 Not Found </Route>
+          </Routes>
+        </Router>
+        <Header />
+      </div>
+    </>
+  );    
 }
+
 
 export default App;
